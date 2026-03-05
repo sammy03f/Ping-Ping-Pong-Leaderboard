9396cbd5014ebecbf7db7232603e84b87714278b
Samuel Fisher <samuelfisher@berkeley.edu>
2026-03-05 13:33:37 -0800

    adding joe

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let players = [
 {name:"JJ", rating:1000},
 {name:"Ramji", rating:1000},
 {name:"Aadi (squid)", rating:1000},
-{name:"Jasper", rating:1000}
+{name:"Jasper", rating:1000},
+{name:"Joe", rating:1000}
 ]
 
 const K = 32
